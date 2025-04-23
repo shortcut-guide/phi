901d5bc3c17ec7dfde37a515e0d1a2e87f5a9c01
shortcut-guide <shortcut.fukui@gmail.com>
2025-04-23 12:18:07 +0900

    update astro dashboard bugfix

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,8 +2,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
+  integrations: [tailwind(), preact(),icon()],
 });
