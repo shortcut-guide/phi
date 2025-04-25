132ea6d20ed9b0bcad608928822a9ba4a73c5673
shortcut-guide <shortcut.fukui@gmail.com>
2025-04-25 18:41:07 +0900

    update logo Dark mode menu

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -4,7 +4,17 @@ import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 import icon from "astro-icon";
 
+// 相対パスに変換するプラグイン
+import relativeLinks from "astro-relative-links";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(),icon()],
+  integrations: [tailwind(), preact(),icon(),relativeLinks()],
+  vite: {
+    resolve: {
+      alias: {
+        '@': '/src'
+      },
+    },
+  },
 });
