84dc25325ec9c0b1850d01f79d5f5aa2e145d277
shortcut <dashio2015@gmail.com>
2025-07-24 12:11:44 +0900

    1211

--- frontend/src/components/amazon/LoginBtnAmazon.tsx
+++ frontend/src/components/amazon/LoginBtnAmazon.tsx
@@ -1,11 +1,20 @@
 // frontend/src/components/LoginBtnAmazon.tsx
-export default function LoginBtnAmazon() {
+import React, { useEffect, useState } from "react";
+import { messages } from "@/f/config/messageConfig";
+
+type AmazonLoginProps = {
+  lang: string;
+};
+
+const AmazonLogin: React.FC<AmazonLoginProps> = ({ lang }) =>{
   return (
     <a
       href="/auth/amazon"
       className="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-600"
     >
-      Amazonでログイン
+      {messages.login?.[lang]?.loginAmazon}
     </a>
   );
-}
+};
+
+export default AmazonLogin;
